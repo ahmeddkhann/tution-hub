91d43fde0271d75bea1c78c9a325118953afae3c
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-10-25 12:24:39 +0500

    work on front end page started

---
+++ src/app/data.js
@@ -0,0 +1,5 @@
+export const details = {
+    title : "Welcome toLearnXplorers Academy",
+   subtitle : "Learn, Explore, and Grow with us",
+   description : "LearnXplorers is an interactive learning platform designed to ignite curiosity and passion for the core subjects of Chemistry, Physics, Biology, Computer Science, and Mathematics. Our mission is to simplify complex concepts and make learning fun through engaging lessons, hands-on activities, and real-world examples. Whether you're a student building foundational knowledge or a curious learner looking to explore the wonders of science and math, LearnXplorers is your go-to resource. Embark on a journey of discovery, where learning knows no bounds!"
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
 "use client"
 
 import React from 'react'
+import { details } from './data'
 
-const page = () => {
+const Page = () => {
   return (
-    <div>
-      BasicTutor
+    <div className="bg-gray-900 text-gray-300 h-screen flex pt-16 justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+          {details.title}
+        </h1>
+        <h2 className="text-2xl md:text-3xl lg:text-4xl mb-2">
+          {details.subtitle}
+        </h2>
+      </div>
+     
     </div>
   )
 }
 
-export default page
+export default Page
